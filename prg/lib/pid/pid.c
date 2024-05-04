ddc589fbfbc1cb7c8e25d3fabb1cdca4b2b35a4d
Skriba <dani.skriba@gmail.com>
2024-05-04 19:21:31 +0200

    PID code compete
    PID source code is complete but not testes.

--- prg/lib/pid/pid.c
+++ prg/lib/pid/pid.c
@@ -21,7 +21,7 @@ float PID_Update(PID *pid, float target, float measurement) {
     float proportional = pid->Kp * error;
 
     // I
-    pid->integrator + 0.5f * pid->Ki * pid->T * (error + pid->lastError);
+    pid->integrator = pid->integrator + 0.5f * pid->Ki * pid->T * (error + pid->lastError);
 
     // Anti-wind-up with integrator clamping
     float intLimMin;
@@ -62,5 +62,25 @@ float PID_Update(PID *pid, float target, float measurement) {
     pid->differentiator = -(2.0f * pid->Kd * (measurement - pid->lastMeasurement)	/* Note: derivative on measurement, therefore minus sign in front of equation! */
                         + (2.0f * pid->tau - pid->T) * pid->differentiator)
                         / (2.0f * pid->tau + pid->T);
+
+    //Compute output
+    pid->out = proportional + pid->integrator + pid->integrator;
+
+    if (pid->out > pid->limMax) {
+
+        pid->out = pid->limMax;
+
+    } else if (pid->out < pid->limMin) {
+
+        pid->out = pid->limMin;
+
+    }
+
+    //Saving 
+    pid->lastError = error;
+    pid->lastMeasurement = measurement;
+
+    //Returning calculated output
+    return pid->out;
 
 }
